f3cd8aa0cd02abc6c58ab7b8ec9f87a567a082bb
KaoriYes <Quinten.kok@hva.nl>
2023-03-23 15:52:53 +0100

    Qt: register ejs files made and added to server

--- server.js
+++ server.js
@@ -14,6 +14,69 @@ app.get('/', (req, res) => {
     res.send('test');
 });
 
+app.post('/submit', async (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+  const secondpassword = req.body.confirm_password;
+  const leerjaar = req.body.leerjaar;
+ 
+  connectDB();
+  if (password !== secondpassword) {
+    res.locals.subtitle = 'Password does NOT match';
+    res.render('student_register.ejs');
+  } else {
+    // res.render('submitted.ejs'); // Qt: this is for later..
+
+    const hashedpw = await bcrypt.hash(password, saltRounds);
+    var userdata = {
+      name,
+      hashedpw,
+      email,
+      leerjaar
+      // no need for : value if key and value are the same
+    };
+    db.collection('Users').insertOne(userdata, function (err, collection) {
+      if (err) throw err;
+      console.log('Record inserted Successfully');
+    });
+
+  }
+});
+
+app.post('/submit-sa', async (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+  const secondpassword = req.body.confirm_password;
+  const leerjaar = req.body.leerjaar;
+  const richting = req.body.richting;
+ 
+  connectDB();
+  if (password !== secondpassword) {
+    res.locals.subtitle = 'Password does NOT match';
+    res.render('student_register.ejs');
+  } else {
+    // res.render('submitted.ejs'); // Qt: this is for later..
+
+    const hashedpw = await bcrypt.hash(password, saltRounds);
+    var userdata = {
+      name,
+      hashedpw,
+      email,
+      leerjaar,
+      richting
+      // no need for : value if key and value are the same
+    };
+    db.collection('Users').insertOne(userdata, function (err, collection) {
+      if (err) throw err;
+      console.log('Record inserted Successfully');
+    });
+
+  }
+});
+
+
 
 app.listen(port, function() {
     console.log('test');
