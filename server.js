d5e096ca0e937eb4ee469d97df8f4d6fcce56fbe
Ufuk <hibbayt@gmail.com>
2023-04-03 15:34:52 +0200

    Like button connected to mongodb

--- server.js
+++ server.js
@@ -6,21 +6,20 @@ const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 const port = process.env.PORT;
+const ObjectId = require("mongodb").ObjectId; // Add this line
 
 app.use(express.static("public"));
 app.use("*/css", express.static("public/css"));
 app.use("*/img", express.static("public/img"));
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("test");
 });
 
 //database
-const {
-  MongoClient,
-  ServerApiVersion
-} = require("mongodb");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 //password in env
 const password = process.env.PASSWORD;
@@ -40,28 +39,26 @@ const client = new MongoClient(uri, {
 const databaseStuds = client.db("studsdb");
 const collectionStuds = databaseStuds.collection("col_studs");
 
-// route voor matchpage
-const studsSchema = {
-  studsnaam: "string",
-  studsleerjaar: "string",
-  vakken: "array",
-};
-
 app.get("/matchpage", async (req, res) => {
   const studs = await collectionStuds.find().toArray();
   res.render("MatchPage.ejs", {
-    studs: studs
+    studs: studs,
   });
 });
 
 // route om stud te liken
-app.get('/like-stud/:id', function (req, res) {
-  var id = req.params.id;
-  // Update the "liked" value of the stud with the given ID in the MongoDB database
-  // ...
-  res.send('OK');
+app.post("/studs/:id", function (req, res) {
+  var studId = req.params.id;
+  collectionStuds.findOneAndUpdate(
+    { _id: new ObjectId(studId) }, // modified this line
+    { $set: { liked: req.body.liked === "true" } },
+    (err, result) => {
+      if (err) return res.send(err);
+      res.send(result);
+    }
+  );
 });
 
-app.listen(port, function () {
-  console.log(port);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
