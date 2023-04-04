9662b5a54d7ff67fc5cd6228d888acf2047208d7
Ufuk <hibbayt@gmail.com>
2023-04-04 03:41:37 +0200

    Profiles are likeable

--- server.js
+++ server.js
@@ -6,20 +6,27 @@ const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 const port = process.env.PORT;
-const ObjectId = require("mongodb").ObjectId;
+const {
+  ObjectId
+} = require('mongodb');
 
 app.use(express.static("public"));
 app.use("*/css", express.static("public/css"));
 app.use("*/img", express.static("public/img"));
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 app.get("/", (req, res) => {
   res.send("test");
 });
 
 //database
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const {
+  MongoClient,
+  ServerApiVersion
+} = require("mongodb");
 
 //password in env
 const password = process.env.PASSWORD;
@@ -39,12 +46,27 @@ const client = new MongoClient(uri, {
 const databaseStuds = client.db("studsdb");
 const collectionStuds = databaseStuds.collection("col_studs");
 
+
+//route naar de matchpage
 app.get("/matchpage", async (req, res) => {
   const studs = await collectionStuds.find().toArray();
   res.render("MatchPage.ejs", {
     studs: studs,
   });
 });
+
+app.post("/like", async (req, res) => {
+  const studId = req.body.studId;
+  await collectionStuds.updateOne({
+    _id: new ObjectId(studId)
+  }, {
+    $set: {
+      liked: true
+    }
+  });
+  res.redirect("/matchpage");
+});
+
 
 app.listen(port, function () {
   console.log(port);
