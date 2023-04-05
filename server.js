ceac29cfece9b4236cb62fb3f334dd02ff35f859
Ufuk <hibbayt@gmail.com>
2023-04-05 18:56:04 +0200

    Ufuk | fixed likedstuds routing user session

--- server.js
+++ server.js
@@ -525,6 +525,8 @@ app.post("/nextPage", async (req, res) => {
 
 //route naar liked studs
 app.get("/likedstuds", async (req, res) => {
+  const user1 = req.session.user.email;
+  const user = await collectionUsers.findOne({ email: user1 });
   const studs = await collectionStuds
     .find({
       liked: true,
@@ -532,6 +534,7 @@ app.get("/likedstuds", async (req, res) => {
     .toArray();
   res.render("likedStuds.ejs", {
     studs: studs,
+    user,
   });
 });
 
