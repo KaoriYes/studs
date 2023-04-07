ae35bb230c8d74ccc41bc88ffba5ed15d0523c89
KaoriYes <Quinten.kok@hva.nl>
2023-04-07 13:08:33 +0200

    Qt: fixed everything

--- routes/chatroute.js
+++ routes/chatroute.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const app = express();
+const session = require("express-session");
+const MongoDBSession = require("express-mongodb-session")(session);
 const Chats = require("../models/chatSchema");
 const router = express.Router();
-// const chatController = require("../controllers/chatController");
+const http = require("http").Server(app);
+const io = require("socket.io");
+const sharedSession = require("express-socket.io-session");
 
 //database verbinden
 const { MongoClient, ServerApiVersion } = require("mongodb");
@@ -17,6 +22,33 @@ const client = new MongoClient(uri, {
 const databaseUsers = client.db("studsdb");
 const collectionUsers = databaseUsers.collection("col_users");
 
+//session store
+const store = new MongoDBSession({
+  uri: uri,
+  collection: "col_sessions",
+  databaseName: "studsdb",
+});
+const secret = process.env.SECRET;
+const session1 = session({
+  secret: secret,
+  cookie: {
+    maxAge: 2592000000,
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: store,
+});
+
+//integrating socketio
+socket = io(http);
+//session
+socket.use(
+  sharedSession(session1, {
+    autoSave: true,
+  })
+);
+//database connection
+// const Chat = require("./models/chatSchema");
 
 const checkChat = async (req, res, next) => {
    const chat1id = req.params.chats;
@@ -49,4 +81,53 @@ router.route("/:chats").get( checkChat, async (req, res) => {
     res.status(404).send("Chat not found");
   }
 });
+
+
+//setup event listener
+socket.on("connection", async (socket) => {
+  console.log("user connected");
+  console.log(socket.handshake.session.user.name);
+
+  socket.on("disconnect", function () {
+    console.log("user disconnected");
+  });
+
+  //Somebody is typing
+  socket.on("typing", (data) => {
+    socket.broadcast.emit("notifyTyping", {
+      user: data.user,
+      message: data.message,
+    });
+  });
+
+  //when somebody stops typing
+  socket.on("stopTyping", () => {
+    socket.broadcast.emit("notifyStopTyping");
+  });
+
+  socket.on("chat message", async function (msg) {
+    console.log("message: " + msg);
+
+    //broadcast message to everyone in port except yourself.
+    socket.broadcast.emit("received", {
+      message: msg,
+    });
+
+    //saves chat to the database
+    connect.then(async (db) => {
+      const user1 = socket.handshake.session.user.name;
+      const user = await collectionUsers.findOne({
+        name: user1,
+      });
+      let chatMessage = new Chat({
+        message: msg,
+        sender: user.name,
+      });
+
+      chatMessage.chatID = user.chats;
+      await chatMessage.save();
+    });
+  });
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -5,30 +5,23 @@ const app = express();
 const session = require("express-session");
 const MongoDBSession = require("express-mongodb-session")(session);
 const bodyParser = require("body-parser");
-const bcrypt = require("bcryptjs");
-const saltRounds = 12;
 require("dotenv").config();
 const mongoose = require("mongoose");
 mongoose.Promise = require("bluebird");
-
-const sharedSession = require("express-socket.io-session");
 const compression = require("compression");
-
-//require the http module
 const http = require("http").Server(app);
-// require the socket.io module
-const io = require("socket.io");
 
 //bodyparser middleware
 app.use(express.json());
 
 const port = 1337;
 const methodOverride = require("method-override");
-const { v4: uuidv4 } = require("uuid");
 const multer = require("multer");
 const upload = multer({
   dest: "public/uploads/",
 });
+
+app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 var path = require("path");
@@ -37,15 +30,13 @@ app.use("*/css", express.static("public/css"));
 app.use("*/img", express.static(path.join(__dirname, "public/img")));
 app.use("*/scripts", express.static(path.join(__dirname, "public/scripts")));
 
-app.set("view engine", "ejs");
-app.use("/public/", express.static("./public"));
 app.use(methodOverride("_method"));
-var path = require("path");
-app.use(express.static(path.join(__dirname, "public")));
 
 //database verbinden
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const { addAbortSignal } = require("stream");
+const {
+  MongoClient,
+  ServerApiVersion
+} = require("mongodb");
 require("dotenv").config();
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, {
@@ -54,16 +45,7 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-//vakken datsase
-const databaseVakken = client.db("studsdb");
-const collectionVakken = databaseVakken.collection("col_vakken");
-//users database
-const databaseUsers = client.db("studsdb");
-const collectionUsers = databaseUsers.collection("col_users");
 
-//col voor de studs
-const databaseStuds = client.db("studsdb");
-const collectionStuds = databaseStuds.collection("col_studs");
 
 //session store
 const store = new MongoDBSession({
@@ -88,17 +70,10 @@ const checkLogin = (req, res, next) => {
   if (req.session.user) {
     next();
   } else {
-    res.redirect("/preRegister");
+    res.redirect("/account/preRegister");
   }
 };
 
-const checkLoggedin = (req, res, next) => {
-  if (req.session.user) {
-    res.redirect("back");
-  } else {
-    next();
-  }
-};
 const connect = mongoose.connect(uri, {
   dbName: "studsdb",
   useNewUrlParser: true,
@@ -114,12 +89,13 @@ connect.then(
 );
 
 module.exports = connect;
+
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 );
-
+//Routes:
 app.get("/", checkLogin, async (req, res) => {
   res.redirect("/matchpage");
 });
@@ -133,66 +109,10 @@ app.use("/account", require("./routes/qtLoginRegister"));
 app.use("/filter", require("./routes/bartFilter"));
 app.use("/nextPage", require("./routes/bartFilter"));
 app.use("/theme", require("./routes/aliTheme"));
-app.use("/chats",  require("./routes/chatroute"));
+app.use("/chats", require("./routes/chatroute"));
 
 
-//integrating socketio
-socket = io(http);
-//session
-socket.use(
-  sharedSession(session1, {
-    autoSave: true,
-  })
-);
-//database connection
-const Chat = require("./models/chatSchema");
 
-//setup event listener
-socket.on("connection", async (socket) => {
-  console.log("user connected");
-  console.log(socket.handshake.session.user.name);
-
-  socket.on("disconnect", function () {
-    console.log("user disconnected");
-  });
-
-  //Somebody is typing
-  socket.on("typing", (data) => {
-    socket.broadcast.emit("notifyTyping", {
-      user: data.user,
-      message: data.message,
-    });
-  });
-
-  //when somebody stops typing
-  socket.on("stopTyping", () => {
-    socket.broadcast.emit("notifyStopTyping");
-  });
-
-  socket.on("chat message", async function (msg) {
-    console.log("message: " + msg);
-
-    //broadcast message to everyone in port except yourself.
-    socket.broadcast.emit("received", {
-      message: msg,
-    });
-
-    //saves chat to the database
-    connect.then(async (db) => {
-      const user1 = socket.handshake.session.user.name;
-      const user = await collectionUsers.findOne({
-        name: user1,
-      });
-      let chatMessage = new Chat({
-        message: msg,
-        sender: user.name,
-      });
-
-      chatMessage.chatID = user.chats;
-      await chatMessage.save();
-    });
-  });
-});
 http.listen(port, () => {
   console.log("Running on Port: " + port);
 });
